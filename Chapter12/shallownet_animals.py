79cf8389e544e2cbaf63f65e7c67e96f3253770a
Patrick Musau <pmusau13ster@gmail.com>
2020-01-27 15:19:51 -0600

    80 percent accuracy on dogs and cats

--- Chapter12/shallownet_animals.py
+++ Chapter12/shallownet_animals.py
@@ -47,6 +47,9 @@ data=data.astype("float")/255.0
 #convert the labels from integers to vectors
 trainY=LabelBinarizer().fit_transform(trainY)
 testY=LabelBinarizer().fit_transform(testY)
+#this is me forcing them to take the softmax format
+trainY=np.hstack((trainY, 1 - trainY))
+testY=np.hstack((testY, 1 - testY))
 
 #initialize the optimizer and model 
 
@@ -60,21 +63,26 @@ model=ShallowNet.build(width=32,height=32,depth=3,classes=2)
 model.compile(loss="categorical_crossentropy",optimizer=opt,metrics=['accuracy'])
 
 print("[INFO] training network....")
-H=model.fit(trainX,trainY,validation_data=(testX,testY),batch_size=256,epochs=100,verbose=1)
 
+#specify the number of epochs
+num_epochs=200
+
+H=model.fit(trainX,trainY,validation_data=(testX,testY),batch_size=256,epochs=num_epochs,verbose=1)
+print(H.history)
 #evaluate the network
 print("INFO evaluating network...")
 predictions=model.predict(testX,batch_size=64)
+print(predictions[0])
 print(classification_report(testY.argmax(axis=1),predictions.argmax(axis=1),target_names=["cat","dog"]))
 
 #plot the training loss and accuracy
 
 plt.style.use("ggplot")
 plt.figure()
-plt.plot(np.arange(0, 100), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, 100), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, 100), H.history["acc"], label="train_acc")
-plt.plot(np.arange(0, 100), H.history["val_acc"], label="val_acc")
+plt.plot(np.arange(0, num_epochs), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, num_epochs), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, num_epochs), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, num_epochs), H.history["val_accuracy"], label="val_acc")
 plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
