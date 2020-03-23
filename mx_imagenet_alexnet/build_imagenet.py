ac252d0d4e005d48f891eb0b690a2da11c7e1f76
Patrick Musau <pmusau13ster@gmail.com>
2020-03-23 18:01:58 -0500

    dataset building

--- mx_imagenet_alexnet/build_imagenet.py
+++ mx_imagenet_alexnet/build_imagenet.py
@@ -1,13 +1,43 @@
 #import the neccessary packages
+
+#packages we just defined
 import config.imagenet_alexnet_config as config
 from utils.imagenethelper import ImageNetHelper
 
-if __name__=="__main__":
+#other incredibly helpful packages
+from sklearn.model_selection import train_test_split
+import numpy as np  
+import progressbar
+import json
+import cv2
 
-    imhelper=ImageNetHelper(config)
-    #print(imhelper.human_mappings,imhelper.labelMappings)
-    #print(imhelper.valBlacklist)
-    im_paths,im_labels=imhelper.buildTrainingSet()
-    print(len(im_paths),len(im_labels))
-    val_paths,val_labels=imhelper.buildValidationSet()
-    print(len(val_paths),len(val_labels))
+# initialize the ImageNet helper and use it to construct the set 
+# of training and testing data
+imhelper=ImageNetHelper(config)
+train_paths,train_labels=imhelper.buildTrainingSet()
+print("[INFO]","Total Image Paths:",len(train_paths),"Total Labels",len(train_labels))
+val_paths,val_labels=imhelper.buildValidationSet()
+print("[INFO]","Total Validation Paths:",len(val_paths),"Total Labels",len(val_labels))
+
+# perform stratified sampling from the training set to construct a 
+# a test set
+
+print("[INFO] constructing splits...")
+
+split = train_test_split(train_paths,train_labels,test_size=config.NUM_TEST_IMAGES,stratify=train_labels,random_state=42)
+
+(trainPaths,testPaths,trainLabels,testLabels)=split
+
+# construct a list pairing the training, validation, and testing 
+# image paths along with their corresponding labels and output list files
+
+datasets = [
+    ("train",trainPaths,trainLabels,config.TRAIN_MX_LIST),
+    ("val",val_paths, val_labels,config.VAL_MX_LIST),
+    ("test",testPaths,testLabels,config.TEST_MX_LIST)
+]
+
+# initialize the list of Red, Green, and Blue averages
+(R,G,B) = ([],[],[])
+
+
